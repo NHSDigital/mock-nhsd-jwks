4189bc61d1547c2b682298f3ba1e45eea199c586
Matthew Clark <matthew.clark21@nhs.net>
2021-06-22 15:44:53 +0100

    AMB-736 changed from json to json()

--- api_tests/user_role_shared_flow/test_cases.py
+++ api_tests/user_role_shared_flow/test_cases.py
@@ -89,4 +89,4 @@ class TestCasesSuite:
 
         # Then
         assert_that(expected_status_code).is_equal_to(response.status_code)
-        assert_that(response.json).is_not_equal_to({})
+        assert_that(response.json()).is_not_equal_to({})
